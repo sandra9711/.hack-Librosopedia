b135b7427dfe8769feb1f763e7f43d9b4f2d7b27
unknown <sandrarehan97@gmail.com>
2021-01-28 22:35:50 +0530

    function changed

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 function myFun(){
     const btn=document.getElementById("bars");
     const list=document.getElementById("list-res");
@@ -11,4 +12,38 @@ function myFun(){
             event.preventDefault()
         }
     })
+}
+function myFunction(){
+    if(document.getElementById("eid").value==null){
+        window.alert("This field can't be empty");
+    }
+    if(document.getElementById("fnm").value==null){
+        window.alert("This field can't be empty");
+    }
+    if(document.getElementById("snm").value==null){
+        window.alert("This field can't be empty");
+    }
+    if(document.getElementById("pno").value==null){
+        window.alert("This field can't be empty");
+    }
+    if(document.getElementById("psw").value==null){
+        window.alert("This field can't be empty");
+    }
+    if(document.getElementById("cpw").value==null){
+        window.alert("This field can't be empty");
+    }
+    fn2();
+    loc();
+}
+function loc(){
+    location.replace("library.html")
+}
+function fn2(){
+    var psw=document.getElementById("pws").value;
+    var cpw=document.getElementById("cpw").value;
+    if(psw!=cpw){
+        window.alert("Invalid password")
+    }else{
+        document.write("<h2 style='padding:20px;'>Welcome ,You Succesfully Joined Librosopedia community.<br> <br><button style='background-color:#6d5f5f;color:white; padding:10px;' class='btn btn-secondary' onclick='loc()'>Get Started</button>")
+    }
 }
