2453366d8530e4db8ea7e41f8070e3f1fcb97da1
SANDRA <sandrarehan97@gmail.com>
2021-03-12 19:41:24 +0530

    firebase added

---
+++ login.js
@@ -0,0 +1,65 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAZ63jV93s9PJacblbiLnMeo7UcUg1nuu4",
+    authDomain: "librosopedia.firebaseapp.com",
+    projectId: "librosopedia",
+    storageBucket: "librosopedia.appspot.com",
+    messagingSenderId: "956548396826",
+    appId: "1:956548396826:web:49a24f9f495a41ce111ea3"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  //const auth = firebase.auth();
+  function signup(){
+    var email = document.getElementById("eid");
+    var pasw = document.getElementById("psw");
+    firebase.auth().createUserWithEmailAndPassword(email, pasw)
+  .then((userCredential) => {
+    // Signed in 
+    var user = userCredential.user;
+    alert("Yay Signed Up");
+    location.replace("library.html");
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ..
+  });
+    
+
+  }
+  
+  
+firebase.auth.Auth.Persistence.NONE;
+  
+  function login(){
+      var email = document.getElementById("reqe").value;
+      var pasw = document.getElementById("reqp").value;
+      
+      const promise = auth.signInWithEmailAndPassword(email, pasw).then((success) =>{
+        
+        var user = userCredential.user;
+            var uid;
+            if (user != null) {
+                uid = user.uid;
+            }
+            
+		alert("Logged In");
+        location.replace("library.html");
+    });
+		promise.catch(e => alert(e.message));
+		
+		alert("Signed In");
+
+  }
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/firebase.User
+      var uid = user.uid;
+      // ...
+    } else {
+      // User is signed out
+      // ...
+    }
+  });
