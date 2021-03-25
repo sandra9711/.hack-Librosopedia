08d8afb9acf05dcd53cd179c6251435eea328af3
SANDRA <sandrarehan97@gmail.com>
2021-03-25 18:52:13 +0530

    db comp

--- public/JS/login.js
+++ public/JS/login.js
@@ -22,7 +22,52 @@
     
 
   }
-  
+function data_fn(){
+    var d_1=document.getElementById("fid_txt1");
+    var d_2=document.getElementById("fid_txt2");
+    var d_3=document.getElementById("fid_txt3");
+    var d_5=document.getElementById("fid_txt5");
+   if(d_1.value==""){
+        alert("Contributor Name should be filled.");
+    }
+    if(d_2.value==""){
+        alert("Book Name and category should be filled.");
+
+    }
+    if(d_3.value==""){
+        alert("Contact cannot be empty.");
+    }
+    
+    //writeUserData(d_1, d_2, d_3,d_5);
+    var fref=firebase.database().ref('book data');
+    if(fref==null){
+      console.log("Error")
+    }else{
+      console.log("not null")
+    }
+    fref.set({
+      name: d_1,
+      book:d_2,
+      contact:d_3,
+      price: d_5
+    });
+    let bRef = this.database.ref('users/' + d_2);
+    bRef.child('cond/'+d_1).set({'contact': value.d_1, 'price': value.d_5});
+    //fref.ref("contributor").child("price").set(d_5.value);
+    alert("Thank You.Submission succesful.Our team will review the data.")
+    document.getElementById('form').reset();
+}
+function writeUserData(d_1, d_2, d_3,d_5) {
+  firebase.database().ref('books/' + d_2).set({
+    Name: d_1.value,
+    book : d_2.value,
+    contact : d_3.value,
+    price : d_5.value
+  });
+  alert("Thank You.Submission succesful.Our team will review the data.")
+  document.getElementById('form').reset();
+}
+
   function google_login(){
     const provider = new firebase.auth.GoogleAuthProvider();
     firebase.auth().signInWithPopup(provider).then(result => {
